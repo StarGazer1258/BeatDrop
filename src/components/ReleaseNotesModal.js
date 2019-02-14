f676fe5777f642951397ab9c347328c96dfcbda2
StarGazer1258 <blackbandit1258@gmail.com>
2019-02-14 13:50:50 -0600

    Update release notes for build 2.0.13-beta

--- src/components/ReleaseNotesModal.js
+++ src/components/ReleaseNotesModal.js
@@ -23,22 +23,12 @@ class ReleaseNotesModal extends Component {
           <h2 style={{color: 'lightgreen'}}>What's new?</h2>
           <hr style={{borderColor: 'lightgreen'}} />
           <ul>
-            <li>Added failure case for sharing externally downloaded songs.</li>
-            <li>Added this fancy new release notes dialog!</li>
-            <li>Added auto-update support for beta builds.</li>
-          </ul>
-          <h2 style={{color: 'gold'}}>What's changed?</h2>
-          <hr style={{borderColor: 'gold'}} />
-          <ul>
-            <li>Highlight color on view tabs is now BeatDrop Blue.</li>
-            <li>Loading animations have a cool new look!</li>
+            <li>Multiple songs can now be downloaded in a single link.<br /><i>e.g. beatdrop://songs/download/31-11,1183-814,811-535,27-9,517-321</i></li>
           </ul>
           <h2 style={{color: 'salmon'}}>What's fixed?</h2>
           <hr style={{borderColor: 'salmon'}} />
           <ul>
-            <li>Fixed bug where app would crash if more than 3 songs were queued to download.</li>
-            <li>Fixed bug where "Add to Playlist" button would do nothing if song was downloaded externally.</li>
-            <li>Fixed bug where sidebar fails to close on first click.</li>
+            <li>Auto-updates should finally work for beta builds.</li>
           </ul>
           <br />
           <Button type="primary" onClick={() => { this.props.setLatestReleaseNotes(require('../../package.json').version) }}>Awesome!</Button>
