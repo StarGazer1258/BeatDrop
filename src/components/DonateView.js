a089bf7b4768ca7207dff016155ac1b4af928f4d
StarGazer1258 <blackbandit1258@gmail.com>
2019-03-20 19:47:49 -0500

    Change donation page to Patreon

--- src/components/DonateView.js
+++ src/components/DonateView.js
@@ -2,21 +2,18 @@ import React, { Component } from 'react'
 import '../css/DonateView.scss'
 
 import Heart from '../assets/heart-100.png'
-import Button from './Button';
+import PatreonButton from '../assets/become-a-patron-button.png'
 
 class DonateView extends Component {
 
   render() {
     return (
       <div id="donate-view">
-        <img src={ Heart } alt="Heart"/>
+        <img className="heart" src={ Heart } alt="Heart"/>
         <h1>Donate to BeatDrop!</h1>
-        <p>We work hard to make BeatDrop the best it can be, but we don't always have the time and money to do so.
+        <p>I work hard to make BeatDrop the best it can be, but I don't always have the time and money to do so.
           If you could chip in just a little bit, it's makes working on BeatDrop all that more worthwhile!</p>
-        <b>Recommended Amounts:</b>
-        <Button type="primary" onClick={ () => { window.require('electron').shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y346GFENN5WUC') } }>Donate $10 Once</Button><div className="flex-br"></div>
-        <Button type="destructive" onClick={ () => { window.require('electron').shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Z2UUWMFZXLYXN') } }>Donate $5 Monthly</Button><div className="flex-br"></div>
-        <Button onClick={ () => { window.require('electron').shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=U2JJM42HAF7PN') } }>Custom Donation</Button><div className="flex-br"></div>
+          <img className="patreon-button"  src={ PatreonButton } alt="Become a Patron" onClick={ () => { window.require('electron').shell.openExternal('https://www.patreon.com/bePatron?u=18487054') } }/>
       </div>
     )
   }
